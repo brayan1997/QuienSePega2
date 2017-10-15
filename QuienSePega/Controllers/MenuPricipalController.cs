c0e85f33534d3bec890f8f83f8d9565de6600229
brayan1997 <bsotelo@unipanamericana.edu.co>
2017-10-15 15:44:11 -0500

    Initial commit

---
+++ QuienSePega/Controllers/MenuPricipalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuienSePega.Controllers
+{
+    public class MenuPricipalController : Controller
+    {
+        // GET: MenuPricipal
+        public ActionResult MenuPrincipal()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuienSePega/Controllers/cursoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuienSePega.Controllers
+{
+    public class cursoController : Controller
+    {
+        // GET: curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult cursoHtml()
+        {
+            return View();
+        }
+
+    }
+}
